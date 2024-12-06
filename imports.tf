25501c72805065f289bace29a828593f1ab015ff
Richard Leaver <richard.leaver@gmail.com>
2024-12-06 16:42:49 -0600

    Initial commit

---
+++ imports.tf
@@ -0,0 +1,48 @@
+##################################################################################
+# IMPORTS
+##################################################################################
+
+#import {
+#  to = module.main.aws_vpc.this[0]
+#  id = "vpc-0a9b5bca20f9563dc" #VPC
+#}
+
+#import {
+#  to = module.main.aws_subnet.public[0]
+#  id = "subnet-07a5467e6cdc0ab2a" #PublicSubnet1
+#}
+
+#import {
+#  to = module.main.aws_subnet.public[1]
+#  id = "subnet-00a4b63fb9c90ec61" #PublicSubnet2
+#}
+
+#import {
+#  to = module.main.aws_internet_gateway.this[0]
+#  id = "igw-0c318b697eb1bcd42" #InternetGateway
+#}
+
+#import {
+#  to = module.main.aws_route.public_internet_gateway[0]
+#  id = "rtb-00068adbab8a6e5d3_0.0.0.0/0" #DefaultPublicRoute
+#}
+
+#import {
+#  to = module.main.aws_route_table.public[0]
+#  id = "rtb-00068adbab8a6e5d3" #PublicRouteTable
+#}
+
+#import {
+#  to = module.main.aws_route_table_association.public[0]
+#  id = "subnet-07a5467e6cdc0ab2a/rtb-00068adbab8a6e5d3" #PublicSubnet1/PublicRouteTable
+#}
+
+#import {
+#  to = module.main.aws_route_table_association.public[1]
+#  id = "subnet-00a4b63fb9c90ec61/rtb-00068adbab8a6e5d3" #PublicSubnet2/PublicRouteTable
+#}
+
+#import {
+#  to = aws_security_group.ingress
+#  id = "sg-052773f38c90aeba4" #NoIngressSecurityGroup
+#}
